eae860b7049a7978429a3dd263bcccda9f8c10cb
DESKTOP-08Q8MT8\Bon <opina.bon30@gmail.com>
2021-10-21 13:44:31 +0800

    Fixed Footer not aligned with main ang header

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,34 +3,36 @@ import Facebook from '@assets/icons/facebook';
 import Github from '@assets/icons/github';
 import Instagram from '@assets/icons/instagram';
 import Twitter from '@assets/icons/twiitter';
-import { Flex, HStack, Text } from '@chakra-ui/react';
+import { Container, Flex, HStack, Text } from '@chakra-ui/react';
 import { FC } from 'react';
 
 const Footer: FC = () => {
   return (
-    <Flex as="footer" background="#fff" padding="20px 112px" justifyContent="space-between" flexShrink={0}>
-      <Text fontWeight="400" color="gray.400">
-        © {new Date().getFullYear()} HOV Onboarding. All rights reserved.{' '}
-      </Text>
+    <Container display="flex" maxW="1220px" padding={0}>
+      <Flex width="100%" as="footer" background="#fff" padding="20px 0px" justifyContent="space-between" flexShrink={0}>
+        <Text fontWeight="400" color="gray.400">
+          © {new Date().getFullYear()} HOV Onboarding. All rights reserved.{' '}
+        </Text>
 
-      <HStack spacing="28px">
-        <a rel="noreferrer" href="https://www.facebook.com/highoutputhq/" target="_blank">
-          <Facebook />
-        </a>
-        <a rel="noreferrer" href="https://www.instagram.com" target="_blank">
-          <Instagram />
-        </a>
-        <a rel="noreferrer" href="https://twitter.com/highoutputhq" target="_blank">
-          <Twitter />
-        </a>
-        <a rel="noreferrer" href="https://github.com" target="_blank">
-          <Github />
-        </a>
-        <a rel="noreferrer" href="https://dribbble.com/" target="_blank">
-          <Dribble />
-        </a>
-      </HStack>
-    </Flex>
+        <HStack spacing="28px">
+          <a rel="noreferrer" href="https://www.facebook.com/highoutputhq/" target="_blank">
+            <Facebook />
+          </a>
+          <a rel="noreferrer" href="https://www.instagram.com" target="_blank">
+            <Instagram />
+          </a>
+          <a rel="noreferrer" href="https://twitter.com/highoutputhq" target="_blank">
+            <Twitter />
+          </a>
+          <a rel="noreferrer" href="https://github.com" target="_blank">
+            <Github />
+          </a>
+          <a rel="noreferrer" href="https://dribbble.com/" target="_blank">
+            <Dribble />
+          </a>
+        </HStack>
+      </Flex>
+    </Container>
   );
 };
 
